59cf25e84f58a5a3938efb274c3da0d12ba9833a
Jared Kobos <jaredkobos@gmail.com>
2017-10-01 21:44:34 -0400

    Create Timer component

---
+++ src/components/Timer.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>{{count}}</div>
+</template>
+
+<script>
+import Timer from '../../node_modules/easytimer/dist/easytimer.min.js'
+
+const timer = new Timer()
+
+export default {
+  name: 'timer',
+  props: ['count', 'seconds'],
+  data () {
+    return {
+      completed: 0
+    }
+  },
+  methods: {
+    processTick: function () {
+      this.completed++
+    },
+    startTimer: function () {
+      timer.start()
+    }
+  },
+  computed: {
+    remaining: function () {
+      return this.count - this.completed
+    }
+  },
+  mounted () {
+    console.log(true)
+  }
+
+}
+</script>
+
+<style>
+</style>
