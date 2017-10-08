143bd70ac9641cb3307003d41052c130019f69d8
Jared Kobos <jaredkobos@gmail.com>
2017-10-07 20:21:28 -0400

    Functional basic timer
    Removed dependency on EasyTimer.js because the event hooks weren't
    working. Replaced with vanilla JS setInterval() function.

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,36 +1,43 @@
 <template>
-  <div>{{timeElapsed}}</div>
+  <div>{{seconds}}</div>
 </template>
 
 <script>
-import Timer from '../../node_modules/easytimer/dist/easytimer.min.js'
-
-const timer = new Timer()
 
 export default {
   name: 'timer',
   props: ['count', 'seconds'],
   data () {
     return {
-      completed: 0
+      completed: 0,
       timeElapsed: ''
     }
   },
   methods: {
-    processTick: function () {
-      this.completed++
+    tick: function () {
+      this.timeElapsed = Date.now() - this.start
     },
-    startTimer: function () {
-      timer.start()
-      timer.addEventListener('secondsUpdated', function(e) {
-        this.timeElapsed = timer.getTimeValues().toString()
-      })
+    rep: function () {
+      this.completed++
     }
   },
   computed: {
     remaining: function () {
       return this.count - this.completed
+    },
+    elapsed: function () {
+      return Math.round(this.timeElapsed / 100)
+    },
+    seconds: function () {
+      return (this.elapsed / 10).toFixed(1);
     }
+  },
+  mounted () {
+    this.start = Date.now()
+    this.timer = setInterval(this.tick, 50)
+  },
+  beforeDestroy () {
+    clearInterval(this.timer)
   }
 
 }
